28c175421b0e8d918d809f7bec6995eac650639c
Nikolaos Fragkopoulos <n1co_frago@hotmail.com>
2016-06-03 07:11:22 +0200

    more commit

---
+++ src/oddoccurencesinarray/OddOccurencesInArray.java
@@ -0,0 +1,26 @@
+
+package oddoccurencesinarray;
+
+public class OddOccurencesInArray {
+
+    public static void main(String[] args) {
+
+        int A[] = {9, 5, 5, 6, 3, 6, 3};
+
+        //sets unpaired equal to A[0]. So 9 in this case
+        int unPairedNum = A[0];
+        
+        for (int i = 1; i < A.length; i++) {
+            // An exclusive OR (^), usually abbreviated XOR, will set a bit on if, 
+            //and only if, the bits being compared are different.If they are the same
+            //the the result is 0.
+            // So in this case unpairednum will kinda "remembers" all numbers as
+            //long as they are not repeated twice. When that happens, unpairednum
+            // forgets both numbers.
+            unPairedNum = unPairedNum ^ A[i];
+        }
+
+        System.out.println(unPairedNum);
+    }
+
+}
